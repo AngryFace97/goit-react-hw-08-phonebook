12650c5fc6b504addfdf7ae1a4c3bc28ebfa057b
Paweł Kłodowski <angryface97@gmail.com>
2022-12-22 21:32:38 +0100

    Create NotFound.styled.js

---
+++ src/pages/NotFound/NotFound.styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 450px;
+  margin: 0 auto 10px auto;
+  border: none;
+  border-radius: 20px;
+  background-color: #cfcfcf;
+  -webkit-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.7);
+  -moz-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.7);
+  box-shadow: 0px 0px 20px 5px rgba(0, 0, 0, 0.7);
+`;
+
+export const Bold = styled.b`
+  font-size: 64px;
+`;
+
+export const HomeLink = styled(Link)`
+  text-decoration: none;
+  color: darkred;
+
+  &:hover,
+  &:focus {
+    color: darkviolet;
+  }
+`;
